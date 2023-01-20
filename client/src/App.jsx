5d64bc498378472dc1f9dccc8d40e9ab4235c8c3
hung <hungtrewq3214@gmail.com>
2023-01-20 12:56:34 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return <p>Note app</p>
+}
+
+export default App
---
+++ server/fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+    authors: [
+        {
+            id: 123,
+            name: 'hung',
+        },
+        {
+            id: 999,
+            name: 'linh',
+        },
+    ],
+    folders: [
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2023-1-18T03:42:13Z',
+            authorId: 123,
+        },
+        {
+            id: 2,
+            name: 'New Folder',
+            createdAt: '2023-1-18T03:42:13Z',
+            authorId: 999,
+        },
+        {
+            id: 3,
+            name: 'Work',
+            createdAt: '2023-1-18T03:42:13Z',
+            authorId: 123,
+        },
+    ],
+};
